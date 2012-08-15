c4c593cf2f5424f8b2ff332bbb3386fe5454bcce
paolo <paolo@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2012-08-15 07:59:36 +0000

    manuals. modified OSEK/VDX trademark notice.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/manuals/trunk/manuals@2482 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- ee_com_refman/version.mk
+++ ee_com_refman/version.mk
@@ -1,4 +1,4 @@
 EE_COM_REFMAN__DIR = ee_com_refman
 EE_COM_REFMAN__MAIN_NAME = ee_com_refman
 EE_COM_REFMAN__MAIN = ee_com_refman
-EE_COM_REFMAN__VER = 1_0_0
+EE_COM_REFMAN__VER = 1_0_1
--- ee_refman/version.mk
+++ ee_refman/version.mk
@@ -1,4 +1,4 @@
 EE_REFMAN__DIR = ee_refman
 EE_REFMAN__MAIN_NAME = ee_refman
 EE_REFMAN__MAIN = reference
-EE_REFMAN__VER = 1_4_4
+EE_REFMAN__VER = 1_4_5
